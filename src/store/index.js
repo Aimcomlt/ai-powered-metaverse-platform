2082d76921fad4934ba0ba708fe790866cfe186d
Aime Comeault <robinhoodblock71@gmail.com>
2025-08-04 23:25:53 -0600

    Add event slices

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,33 @@ import { configureStore } from '@reduxjs/toolkit';
 import gtReducer from './gtSlice';
 import taskReducer from './taskSlice';
 import aiReducer from './aiSlice';
+import {
+  governanceTokenEventsReducer,
+  functionalTokenEventsReducer,
+  mpnsRegistryEventsReducer,
+  crossFactionHubEventsReducer,
+  gtStakingEventsReducer,
+  houseOfTheLawEventsReducer,
+  proofOfObservationEventsReducer,
+  pooTaskFlowEventsReducer,
+  genesisBlockFactionEventsReducer,
+  genesisBlockFactoryEventsReducer,
+} from './eventSlices';
 
 export const store = configureStore({
   reducer: {
     gt: gtReducer,
     task: taskReducer,
     ai: aiReducer,
+    governanceTokenEvents: governanceTokenEventsReducer,
+    functionalTokenEvents: functionalTokenEventsReducer,
+    mpnsRegistryEvents: mpnsRegistryEventsReducer,
+    crossFactionHubEvents: crossFactionHubEventsReducer,
+    gtStakingEvents: gtStakingEventsReducer,
+    houseOfTheLawEvents: houseOfTheLawEventsReducer,
+    proofOfObservationEvents: proofOfObservationEventsReducer,
+    pooTaskFlowEvents: pooTaskFlowEventsReducer,
+    genesisBlockFactionEvents: genesisBlockFactionEventsReducer,
+    genesisBlockFactoryEvents: genesisBlockFactoryEventsReducer,
   },
 });
