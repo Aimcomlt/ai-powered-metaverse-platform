2f2ea5c64087d80c7a698c7ed25265903d77c4a0
Aime Comeault <robinhoodblock71@gmail.com>
2025-08-05 14:36:41 -0600

    feat: add ethers provider service

--- src/services/provider.js
+++ src/services/provider.js
@@ -5,8 +5,11 @@ let provider;
 export const getProvider = () => {
   if (provider) return provider;
 
-  if (typeof window !== 'undefined' && window.ethereum) {
-    provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = typeof window !== 'undefined' ? window : {};
+
+  if (ethereum) {
+    // the 'any' network allows the dapp to handle network changes on the fly
+    provider = new ethers.providers.Web3Provider(ethereum, 'any');
   } else {
     const rpcUrl = process.env.RPC_URL || 'http://localhost:8545';
     const chainId = process.env.CHAIN_ID ? parseInt(process.env.CHAIN_ID, 10) : undefined;
